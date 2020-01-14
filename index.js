0c515d379753b2a810d8163a1a005bb7f3731425
asyrofist <rakhasyrofist@gmail.com>
2020-01-14 20:03:41 +0700

    perubahan

--- index.js
+++ index.js
@@ -1,53 +1,89 @@
-const tf = require("@tensorflow/tfjs");
-require('@tensorflow/tfjs-node');
-const iris = require("./iris.json");
-const irisTesting = require("./iris-testing.json");
+/* library & json data */
+const tf = require('@tensorflow/tfjs')
+const train = require('./iris.json')
+const test = require('./iris-testing.json')
 
-const trainingData = tf.tensor2d(iris.map(item => [
-    item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
-]))
-
-const outputData = tf.tensor2d(iris.map(item => [
+/* output data */
+const outputData = tf.tensor2d(
+  train.map(item => [
     item.species === 'setosa' ? 1 : 0,
     item.species === 'virginica' ? 1 : 0,
     item.species === 'versicolor' ? 1 : 0,
-]))
+  ])
+)
+// outputData.print()
 
-const testingData = tf.tensor2d(irisTesting.map(item => [
-    item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
-]))
+/* training data */
+const trainingData = tf.tensor2d(
+  train.map(item => [
+    item.sepal_length,
+    item.sepal_width,
+    item.petal_length,
+    item.petal_width,
+  ])
+)
+// trainingData.print()
 
+/* testing data */
+const test_tensor = tf.tensor2d(
+  test.map(item => [
+    item.sepal_length,
+    item.sepal_width,
+    item.petal_length,
+    item.petal_width,
+  ]),
+  [3, 4]
+)
+// test_tensor.print()
+
+/* modelling data */
 const model = tf.sequential()
-
-model.add(tf.layers.dense({
+model.add(
+  tf.layers.dense({
     inputShape: [4],
-    activation: "sigmoid",
+    activation: 'sigmoid',
     units: 5,
-}))
-
-
-model.add(tf.layers.dense({
+  })
+)
+model.add(
+  tf.layers.dense({
     inputShape: [5],
-    activation: "sigmoid",
+    activation: 'sigmoid',
     units: 3,
-}))
-
-
-model.add(tf.layers.dense({
-    activation: "sigmoid",
+  })
+)
+model.add(
+  tf.layers.dense({
+    activation: 'sigmoid',
     units: 3,
-}))
-
+  })
+)
 model.compile({
-    loss: "meanSquaredError",
-    optimizer: tf.train.adam(0.06)
+  loss: 'meanSquaredError',
+  optimizer: tf.train.adam(0.06),
 })
 
-const startTime = Date.now()
-model.fit(trainingData, outputData, {
-        epochs: 100
-    })
-    .then((history) => {
-    console.log(history);
-        model.predict(testingData).print()
-    })
+/* prediction data */
+model.fit(trainingData, outputData, { epochs: 100 })
+const mi_test = tf.tensor2d([4.1, 2.8, 4.4, 1.5], [1, 4])
+// mi_test.print()
+
+// /* prediction data & log history losses */
+// model.fit(trainingData, outputData, { epochs: 100 }).then(history => {
+//   console.log(history)
+// })
+// const mi_test = tf.tensor2d([4.1, 2.8, 4.4, 1.5], [1, 4])
+// mi_test.print()
+
+/* output prediction */
+const rr = model.predict(mi_test)
+const nuevo = rr.dataSync()
+const new_array = [nuevo[0], nuevo[1], nuevo[2]]
+var indice = nuevo.indexOf(new_array.sort()[2])
+if (indice == 0) {
+  console.log('Iris setosa')
+} else if (indice == 1) {
+  console.log('Iris versicolor')
+} else {
+  console.log('Iris virginica')
+}
